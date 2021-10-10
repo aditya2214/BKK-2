f5f6a8c5ffa024bfca15dd3d0a03e699a2e5de40
aditya2214 <aditokta7333@gmail.com>
2021-10-10 15:39:34 +0700

    make it better

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,12 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/logout', function () {
+    Auth::logout();
+    
+    return view('welcome');
+});
+
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
