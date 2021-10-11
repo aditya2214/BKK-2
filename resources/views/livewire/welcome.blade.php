44cd4a04d935ec2cc97d2e7da70562ca5ae06111
aditya2214 <aditokta7333@gmail.com>
2021-10-11 22:07:41 +0700

    dfdsfds

--- resources/views/livewire/welcome.blade.php
+++ resources/views/livewire/welcome.blade.php
@@ -265,20 +265,20 @@
                         <div class="single-job-items mb-30">
                             <div class="job-items">
                                 <div class="company-img">
-                                    <a href="job_details.html"><img src="frontend/assets/img/logo/logo1.png" alt=""></a>
+                                    <a href="job_details.html"><img src="frontend/assets/img/logo/logo1.png"style="width:100px;"  alt=""></a>
                                 </div>
                                 <div class="job-tittle">
-                                    <a href="job_details.html"><h4>Digital Marketer</h4></a>
+                                    <a href="job_details.html"><h4>{{$vac->title_vacancy}}</h4></a>
                                     <ul>
-                                        <li>Creative Agency</li>
+                                        <!-- <li>Creative Agency</li>
                                         <li><i class="fas fa-map-marker-alt"></i>Athens, Greece</li>
-                                        <li>$3500 - $4000</li>
+                                        <li>$3500 - $4000</li> -->
                                     </ul>
                                 </div>
                             </div>
                             <div class="items-link f-right">
                                 <a href="job_details.html">Full Time</a>
-                                <span>7 hours ago</span>
+                                <span>{{$vac->created_at}}</span>
                             </div>
                         </div>
                         @endforeach
