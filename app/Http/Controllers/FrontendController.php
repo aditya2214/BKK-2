c4b4533b994d6f1e0eda3d46237c34e11e6318d9
aditya2214 <aditokta7333@gmail.com>
2021-10-12 09:35:06 +0700

    livewire add

---
+++ app/Http/Controllers/FrontendController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class FrontendController extends Controller
+{
+    public function index(){
+        $vacancy = \App\Vacancy::all();
+
+        return view('welcome',compact('vacancy'));
+    }
+}
