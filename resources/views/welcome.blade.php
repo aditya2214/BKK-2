71dc75304d5151ccbbeb4e30845f8a25f841d0f2
aditya2214 <aditokta7333@gmail.com>
2021-10-12 16:27:35 +0700

    livewire add

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -11,12 +11,12 @@
 
     <!-- manifest meta -->
     <meta name="apple-mobile-web-app-capable" content="yes">
-    <link rel="manifest" href="manifest.json" />
+    <link rel="manifest" href="frontend/manifest.json" />
 
     <!-- Favicons -->
-    <link rel="apple-touch-icon" href="img/favicon180.png" sizes="180x180">
-    <link rel="icon" href="img/favicon32.png" sizes="32x32" type="image/png">
-    <link rel="icon" href="img/favicon16.png" sizes="16x16" type="image/png">
+    <link rel="apple-touch-icon" href="frontend/img/favicon180.png" sizes="180x180">
+    <link rel="icon" href="frontend/img/favicon32.png" sizes="32x32" type="image/png">
+    <link rel="icon" href="frontend/img/favicon16.png" sizes="16x16" type="image/png">
 
     <!-- Material icons-->
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
