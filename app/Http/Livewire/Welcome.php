33bc4228b91b664bf79b080fe0e2bde03756b949
aditya2214 <aditokta7333@gmail.com>
2021-10-12 09:10:34 +0700

    livewire add

--- app/Http/Livewire/Welcome.php
+++ app/Http/Livewire/Welcome.php
@@ -6,10 +6,16 @@ use Livewire\Component;
 
 class Welcome extends Component
 {
+    public $content = "home";
+
     public function render()
     {
         $vacancy = \App\Vacancy::all();
 
         return view('livewire.welcome',compact('vacancy'));
+    }
+
+    public function job_details(){
+        $this->content = "job_details";
     }
 }
