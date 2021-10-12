4ff31bd4083272692c2590d84c691b1ec3e2d2ff
aditya2214 <aditokta7333@gmail.com>
2021-10-12 17:20:57 +0700

    livewire add

---
+++ resources/views/layouts/app-fontend.blade.php
@@ -0,0 +1,308 @@
+<!doctype html>
+<html lang="en" class="h-100">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <meta name="generator" content="">
+    <title>BKK-Karawang</title>
+
+    <!-- manifest meta -->
+    <meta name="apple-mobile-web-app-capable" content="yes">
+    <link rel="manifest" href="frontend/manifest.json" />
+
+    <!-- Favicons -->
+    <link rel="apple-touch-icon" href="frontend/img/favicon180.png" sizes="180x180">
+    <link rel="icon" href="frontend/img/favicon32.png" sizes="32x32" type="image/png">
+    <link rel="icon" href="frontend/img/favicon16.png" sizes="16x16" type="image/png">
+
+    <!-- Material icons-->
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+
+    <!-- Google fonts-->
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500&display=swap" rel="stylesheet">
+
+    <!-- swiper CSS -->
+    <link href="frontend/vendor/swiper/css/swiper.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="/frontend/css/style.css" rel="stylesheet" id="style">
+</head>
+
+<body class="body-scroll d-flex flex-column h-100 menu-overlay" data-page="homepage">
+    <!-- screen loader -->
+    <div class="container-fluid h-100 loader-display">
+        <div class="row h-100">
+            <div class="align-self-center col">
+                <div class="logo-loading">
+                    <div class="icon icon-100 mb-4 rounded-circle">
+                        <img src="/frontend/img/favicon144.png" alt="" class="w-100">
+                    </div>
+                    <h4 class="text-default">BKK-Karawang</h4>
+                    <p class="text-secondary">Mobile BKK</p>
+                    <div class="loader-ellipsis">
+                        <div></div>
+                        <div></div>
+                        <div></div>
+                        <div></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+
+    <!-- menu main -->
+    <!-- <div class="main-menu">
+        <div class="row mb-4 no-gutters">
+            <div class="col-auto"><button class="btn btn-link btn-40 btn-close text-white"><span class="material-icons">chevron_left</span></button></div>
+            <div class="col-auto">
+                <div class="avatar avatar-40 rounded-circle position-relative">
+                    <figure class="background">
+                        <img src="/frontend/img/user1.png" alt="">
+                    </figure>
+                </div>
+            </div>
+            <div class="col pl-3 text-left align-self-center">
+                <h6 class="mb-1">Errica Johnson</h6>
+                <p class="small text-default-secondary">London, UK</p>
+            </div>
+        </div>
+        <div class="menu-container">
+            <div class="row mb-4">
+                <div class="col">
+                    <h4 class="mb-1 font-weight-normal">$ 1548.00</h4>
+                    <p class="text-default-secondary">My Balance</p>
+                </div>
+                <div class="col-auto">
+                    <button class="btn btn-default btn-40 rounded-circle" data-toggle="modal" data-target="#addmoney"><i class="material-icons">add</i></button>
+                </div>
+            </div>
+
+            <ul class="nav nav-pills flex-column ">
+                <li class="nav-item">
+                    <a class="nav-link active" href="index.html">
+                        <div>
+                            <span class="material-icons icon">account_balance</span>
+                            Home
+                        </div>
+                        <span class="arrow material-icons">chevron_right</span>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="analytics.html">
+                        <div>
+                            <span class="material-icons icon">insert_chart</span>
+                            Analytics
+                        </div>
+                        <span class="arrow material-icons">chevron_right</span>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="refer_friends.html">
+                        <div>
+                            <span class="material-icons icon">perm_contact_calendar</span>
+                            Refer Friends
+                        </div>
+                        <span class="arrow material-icons">chevron_right</span>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="gift_cards.html">
+                        <div>
+                            <span class="material-icons icon">card_giftcard</span>
+                            Gift Cards
+                        </div>
+                        <span class="arrow material-icons">chevron_right</span>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="my_orders.html">
+                        <div>
+                            <span class="material-icons icon">shopping_bag</span>
+                            My Orders
+                        </div>
+                        <span class="arrow material-icons">chevron_right</span>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="setting.html">
+                        <div>
+                            <span class="material-icons icon">settings</span>
+                            Settings
+                        </div>
+                        <span class="arrow material-icons">chevron_right</span>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="pages.html">
+                        <div>
+                            <span class="material-icons icon">layers</span>
+                            Pages
+                        </div>
+                        <span class="arrow material-icons">chevron_right</span>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="controls.html">
+                        <div>
+                            <span class="material-icons icon">widgets</span>
+                            Controls
+                        </div>
+                        <span class="arrow material-icons">chevron_right</span>
+                    </a>
+                </li>
+            </ul>
+            <div class="text-center">
+                <a href="login.html" class="btn btn-outline-danger text-white rounded my-3 mx-auto">Sign out</a>
+            </div>
+        </div>
+    </div>
+    <div class="backdrop"></div> -->
+    
+
+    @yield('content')
+
+    <!-- footer-->
+    <div class="footer">
+        <div class="row no-gutters justify-content-center">
+            <div class="col-auto">
+                <a href="index.html" class="active">
+                    <i class="material-icons">home</i>
+                    <p>Home</p>
+                </a>
+            </div>
+            <div class="col-auto">
+                <a href="analytics.html" class="">
+                    <i class="material-icons">insert_chart_outline</i>
+                    <p>Analytics</p>
+                </a>
+            </div>
+            <div class="col-auto">
+                <a href="wallet.html" class="">
+                    <i class="material-icons">account_balance_wallet</i>
+                    <p>Wallet</p>
+                </a>
+            </div>
+            <div class="col-auto">
+                <a href="shop.html" class="">
+                    <i class="material-icons">shopping_bag</i>
+                    <p>Shop</p>
+                </a>
+            </div>
+            <div class="col-auto">
+                <a href="profile.html" class="">
+                    <i class="material-icons">account_circle</i>
+                    <p>Profile</p>
+                </a>
+            </div>
+        </div>
+    </div>
+
+
+    <!-- color settings style switcher -->
+    <div class="color-picker">
+        <div class="row">
+            <div class="col text-left">
+                <div class="selectoption">
+                    <input type="checkbox" id="darklayout" name="darkmode">
+                    <label for="darklayout">Dark</label>
+                </div>
+                <div class="selectoption mb-0">
+                    <input type="checkbox" id="rtllayout" name="layoutrtl">
+                    <label for="rtllayout">RTL</label>
+                </div>
+            </div>
+            <div class="col-auto">
+                <button class="btn btn-link text-secondary btn-round colorsettings2"><span class="material-icons">close</span></button>
+            </div>
+        </div>
+
+        <hr class="mt-2">
+        <div class="colorselect">
+            <input type="radio" id="templatecolor1" name="sidebarcolorselect">
+            <label for="templatecolor1" class="bg-dark-blue" data-title="dark-blue"></label>
+        </div>
+        <div class="colorselect">
+            <input type="radio" id="templatecolor2" name="sidebarcolorselect">
+            <label for="templatecolor2" class="bg-dark-purple" data-title="dark-purple"></label>
+        </div>
+        <div class="colorselect">
+            <input type="radio" id="templatecolor4" name="sidebarcolorselect">
+            <label for="templatecolor4" class="bg-dark-gray" data-title="dark-gray"></label>
+        </div>
+        <div class="colorselect">
+            <input type="radio" id="templatecolor6" name="sidebarcolorselect">
+            <label for="templatecolor6" class="bg-dark-brown" data-title="dark-brown"></label>
+        </div>
+        <div class="colorselect">
+            <input type="radio" id="templatecolor3" name="sidebarcolorselect">
+            <label for="templatecolor3" class="bg-maroon" data-title="maroon"></label>
+        </div>
+        <div class="colorselect">
+            <input type="radio" id="templatecolor5" name="sidebarcolorselect">
+            <label for="templatecolor5" class="bg-dark-pink" data-title="dark-pink"></label>
+        </div>
+        <div class="colorselect">
+            <input type="radio" id="templatecolor8" name="sidebarcolorselect">
+            <label for="templatecolor8" class="bg-red" data-title="red"></label>
+        </div>
+        <div class="colorselect">
+            <input type="radio" id="templatecolor13" name="sidebarcolorselect">
+            <label for="templatecolor13" class="bg-amber" data-title="amber"></label>
+        </div>
+        <div class="colorselect">
+            <input type="radio" id="templatecolor7" name="sidebarcolorselect">
+            <label for="templatecolor7" class="bg-dark-green" data-title="dark-green"></label>
+        </div>
+        <div class="colorselect">
+            <input type="radio" id="templatecolor11" name="sidebarcolorselect">
+            <label for="templatecolor11" class="bg-teal" data-title="teal"></label>
+        </div>
+        <div class="colorselect">
+            <input type="radio" id="templatecolor12" name="sidebarcolorselect">
+            <label for="templatecolor12" class="bg-skyblue" data-title="skyblue"></label>
+        </div>
+        <div class="colorselect">
+            <input type="radio" id="templatecolor10" name="sidebarcolorselect">
+            <label for="templatecolor10" class="bg-blue" data-title="blue"></label>
+        </div>
+        <div class="colorselect">
+            <input type="radio" id="templatecolor9" name="sidebarcolorselect">
+            <label for="templatecolor9" class="bg-purple" data-title="purple"></label>
+        </div>
+        <div class="colorselect">
+            <input type="radio" id="templatecolor14" name="sidebarcolorselect">
+            <label for="templatecolor14" class="bg-gray" data-title="gray"></label>
+        </div>
+    </div>
+
+
+
+
+    <!-- Required jquery and libraries -->
+    <script src="/frontend/js/jquery-3.3.1.min.js"></script>
+    <script src="/frontend/js/popper.min.js"></script>
+    <script src="/frontend/vendor/bootstrap/js/bootstrap.min.js"></script>
+
+    <!-- cookie js -->
+    <script src="/frontend/js/jquery.cookie.js"></script>
+
+    <!-- Swiper slider  js-->
+    <script src="/frontend/vendor/swiper/js/swiper.min.js"></script>
+
+    <!-- Customized jquery file  -->
+    <script src="/frontend/js/main.js"></script>
+    <script src="/frontend/js/color-scheme-demo.js"></script>
+
+    <!-- PWA app service registration and works -->
+    <script src="/frontend/js/pwa-services.js"></script>
+
+    <!-- page level custom script -->
+    <script src="/frontend/js/app.js"></script>
+</body>
+
+</html>
