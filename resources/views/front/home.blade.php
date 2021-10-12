b78e2829e1ccb26e7ffc6c0331518a0d528df423
aditya2214 <aditokta7333@gmail.com>
2021-10-12 17:53:03 +0700

    livewire add

--- resources/views/front/home.blade.php
+++ resources/views/front/home.blade.php
@@ -1,38 +1,7 @@
 @extends('layouts.app-frontend')
 
 @section('content')
-<!-- Begin page content -->
-<main class="flex-shrink-0 main has-footer">
-    <!-- Fixed navbar -->
-    <header class="header">
-        <div class="row">
-            <!-- <div class="col-auto px-0">
-                <button class="menu-btn btn btn-40 btn-link" type="button">
-                    <span class="material-icons">menu</span>
-                </button>
-            </div> -->
-            <div class="text-left col align-self-center">
-                <a class="navbar-brand" href="#">
-                    <h5 class="mb-0">BKK - Karawang</h5>
-                </a>
-            </div>
-            <div class="ml-auto col-auto pl-0">
-                <button type="button" class="btn btn-link btn-40 colorsettings">
-                    <span class="material-icons">color_lens</span>
-                </button>
-                <!-- 
-                <a href="notification.html" class="menu-btn btn btn-40 btn-link" >
-                    <span class="material-icons">notifications_none</span>
-                    <span class="counter"></span>
-                </a>
-                <a href="profile.html" class="avatar avatar-30 shadow-sm rounded-circle ml-2">
-                    <figure class="m-0 background">
-                        <img src="/frontend/img/user1.png" alt="">
-                    </figure>
-                </a> -->
-            </div>
-        </div>
-    </header>
+
 
     <div class="container mt-3 mb-4 text-center">
         <h2 class="text-white">$ 1,050.00</h2>
@@ -489,5 +458,5 @@
             </div>
         </div>
     </div>
-</main>
+
 @endsection
