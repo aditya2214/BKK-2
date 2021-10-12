031fd26d1f6eca815dce8e283fe4cbba44d5ced0
aditya2214 <aditokta7333@gmail.com>
2021-10-12 15:55:31 +0700

    livewire add

--- app/Http/Controllers/FrontendController.php
+++ app/Http/Controllers/FrontendController.php
@@ -10,36 +10,36 @@ class FrontendController extends Controller
     public function index(){
         $vacancy = \App\Vacancy::all();
 
-        return view('front.home',compact('vacancy'));
+        return view('welcome',compact('vacancy'));
     }
 
-    public function job_details($id){
-        $vacancy_detail = \App\Vacancy::where('id',$id)->first();
+    // public function job_details($id){
+    //     $vacancy_detail = \App\Vacancy::where('id',$id)->first();
 
-        return view('front.job_detail',compact('vacancy_detail'));
-    }
+    //     return view('front.job_detail',compact('vacancy_detail'));
+    // }
 
-    public function daftar(Request $request){
-        return $request->all();
+    // public function daftar(Request $request){
+    //     return $request->all();
 
-    }
+    // }
 
-    public function masukan_kode($id){
-        $cek_kode_vacancy = \App\Vacancy::where('id',$id)->first();
+    // public function masukan_kode($id){
+    //     $cek_kode_vacancy = \App\Vacancy::where('id',$id)->first();
 
-        return view('front.kode_vacancy',compact('cek_kode_vacancy'));
-    }
+    //     return view('front.kode_vacancy',compact('cek_kode_vacancy'));
+    // }
 
-    public function absen(Request $request,$id){
-        $cek_kode_vacancy = \App\Vacancy::where('id',$id)->first();
-        $cek_request = $request->kode_loker;
+    // public function absen(Request $request,$id){
+    //     $cek_kode_vacancy = \App\Vacancy::where('id',$id)->first();
+    //     $cek_request = $request->kode_loker;
 
-        if ($cek_request == $cek_kode_vacancy->kode_vacancy) {
-            Alert::success('Berhasil', 'Silahkan Isi Biodata Anda!!!');
-            return view('front.form_peserta');
-        }else{
-            Alert::error('Gagal', 'Kode Tes Salah!!!');
-            return redirect()->back();
-        }
-    }
+    //     if ($cek_request == $cek_kode_vacancy->kode_vacancy) {
+    //         Alert::success('Berhasil', 'Silahkan Isi Biodata Anda!!!');
+    //         return view('front.form_peserta');
+    //     }else{
+    //         Alert::error('Gagal', 'Kode Tes Salah!!!');
+    //         return redirect()->back();
+    //     }
+    // }
 }
