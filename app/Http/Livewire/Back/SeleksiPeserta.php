3a08feae1ef05b6196bec532e25b02f381492d6d
aditya2214 <aditokta7333@gmail.com>
2021-10-13 21:39:27 +0700

    fdsgfds

--- app/Http/Livewire/Back/SeleksiPeserta.php
+++ app/Http/Livewire/Back/SeleksiPeserta.php
@@ -7,11 +7,11 @@ use Livewire\Component;
 class SeleksiPeserta extends Component
 {
     public $pilih_loker;
-    
+
     public function render()
     {
         $vacancy = \App\Vacancy::all();
-        $get_attendances = \App\Attendance::where('id_vacancy',$id)->get();
+        $get_attendances = \App\Attendance::where('id_vacancy',$this->pilih_loker)->get();
         
         return view('livewire.back.seleksi-peserta',compact('vacancy','get_attendances'));
     }
