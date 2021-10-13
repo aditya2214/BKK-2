9e3aeee015a73ef7a9ba705c946c97a59714d30a
aditya2214 <aditokta7333@gmail.com>
2021-10-13 14:15:09 +0700

    livewire add

--- app/Attendance.php
+++ app/Attendance.php
@@ -6,5 +6,22 @@ use Illuminate\Database\Eloquent\Model;
 
 class Attendance extends Model
 {
-    //
+    protected $fillable = [
+        'id_vacancy',
+        'full_name',
+        'gender',
+        'active_email',
+        'nik',
+        'kabupaten',
+        'address',
+        'place',
+        'date_and_place',
+        'age',
+        'height',
+        'weight',
+        'school',
+        'graduation_year',
+        'experience',
+        'no_handphone'
+    ];
 }
