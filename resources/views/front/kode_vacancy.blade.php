2f2c45233757ad443769a5a35d5a3764485f1c96
aditya2214 <aditokta7333@gmail.com>
2021-10-13 10:43:39 +0700

    livewire add

--- resources/views/front/kode_vacancy.blade.php
+++ resources/views/front/kode_vacancy.blade.php
@@ -9,20 +9,14 @@
         <div class="container">
             <form action="{{ url ('absen/'.$cek_kode_vacancy->id) }}" method="post">
                 @csrf
-                <div class="card">
-                    <div class="card-header border-bottom">
-                        <div class="row">
-                            <div class="col pl-0 align-self-center"  style="text-align:center;" >
-                                <labelfor=""><b>Kode Loker</b></label>
-                                <input type="password" placeholder="Masukan Kode Loker" name="kode_loker" class="form-control">
-                            </div>
-                        </div>
-                    </div>
-                    <div class="card-footer">
-                        <button class="btn btn btn-warning">Masuk</button>
+                <div class="row">
+                    <div class="col pl-0 align-self-center"  style="text-align:center;" >
+                        <labelfor=""><b>Kode Loker</b></label>
+                        <input type="password" placeholder="Masukan Kode Loker" name="kode_loker" class="form-control">
                     </div>
                 </div>
-                        
+                <br>
+                <button class="btn btn btn-warning">Masuk</button>
             </form>
         </div>
     </div>
