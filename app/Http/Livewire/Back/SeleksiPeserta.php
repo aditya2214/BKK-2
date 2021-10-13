296d4dbe99c021cbb569ea1cfab8da3ae6b00d5c
aditya2214 <aditokta7333@gmail.com>
2021-10-13 21:36:31 +0700

    fdsgfds

--- app/Http/Livewire/Back/SeleksiPeserta.php
+++ app/Http/Livewire/Back/SeleksiPeserta.php
@@ -6,10 +6,13 @@ use Livewire\Component;
 
 class SeleksiPeserta extends Component
 {
+    public $pilih_loker;
+    
     public function render()
     {
         $vacancy = \App\Vacancy::all();
+        $get_attendances = \App\Attendance::where('id_vacancy',$id)->get();
         
-        return view('livewire.back.seleksi-peserta',compact('vacancy'));
+        return view('livewire.back.seleksi-peserta',compact('vacancy','get_attendances'));
     }
 }
--- resources/views/livewire/back/seleksi-peserta.blade.php
+++ resources/views/livewire/back/seleksi-peserta.blade.php
@@ -13,15 +13,66 @@
     <div class="col-md-12">
         <div class="card">
             <div class="card-body">
-                <div class="for-group">
-                    <label for="">Pilih Loker</label>
-                    <select name="" id="" class="form-control">
-                            <option class="bg-primary text-white" value="xnxx.com">Pilih</option>
-                        @foreach($vacancy as $vac)
-                            <option value="{{$vac->id}}">{{$vac->title_vacancy}}</option>
-                        @endforeach
-                    </select>
+                <div class="row">
+                    <div class="col-md-6">
+                        <div class="form-group">
+                            <label for="">Pilih Loker</label>
+                            <select wire:model="pilih_loker" required name="" id="" class="form-control">
+                                    <option class="bg-primary text-white" value="">Pilih</option>
+                                @foreach($vacancy as $vac)
+                                    <option value="{{$vac->id}}">{{$vac->title_vacancy}}</option>
+                                @endforeach
+                            </select>
+                        </div>
+                    </div>
                 </div>
+                <hr>
+                @if($pilih_loker == null)
+                <p class="text-danger">Pilih Loker Terlebih Dulu</p>
+                @else
+                <table class="table" id="dataTable">
+                    <thead>
+                        <tr>
+                            <th>No</th>
+                            <th>Nama_Lengkap</th>
+                            <th>Jenis_Kelamin</th>
+                            <th>Email</th>
+                            <th>NIK</th>
+                            <th>Kabupaten</th>
+                            <th>Alamat</th>
+                            <th>TTL</th>
+                            <th>Umur</th>
+                            <th>Tinggi_Badan</th>
+                            <th>Berat_Badan</th>
+                            <th>Asal_Sekolah</th>
+                            <th>Tahun_lulus</th>
+                            <th>Pengalaman</th>
+                            <th>No_Handphone</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach($get_attendances as $key=>$get_attendance)
+                        <tr>
+                            <td>{{$key+1}}</td>
+                            <td>{{$get_attendance->full_name}}</td>
+                            <td>{{$get_attendance->gender}}</td>
+                            <td>{{$get_attendance->active_email}}</td>
+                            <td>{{$get_attendance->nik}}</td>
+                            <td>{{$get_attendance->kabupaten}}</td>
+                            <td>{!!$get_attendance->address!!}</td>
+                            <td>{{$get_attendance->place}},{{$get_attendance->date_and_place}}</td>
+                            <td>{{$get_attendance->age}}</td>
+                            <td>{{$get_attendance->height}}</td>
+                            <td>{{$get_attendance->weight}}</td>
+                            <td>{{$get_attendance->school}}</td>
+                            <td>{{$get_attendance->graduation_year}}</td>
+                            <td>{!!$get_attendance->experience!!}</td>
+                            <td>{{$get_attendance->no_handphone}}</td>
+                        </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+                @endif
             </div>
         </div>
     </div>
