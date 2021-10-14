9aaa7023a1bbc696f278d5d9dceadee195ff19ed
aditya2214 <aditokta7333@gmail.com>
2021-10-14 11:15:22 +0700

    ghghgf

---
+++ database/migrations/2021_10_14_041237_create_absen_sortirs_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAbsenSortirsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('absen_sortirs', function (Blueprint $table) {
+            $table->id();
+            $table->integer('id_peserta');
+            $table->string('kategori_tes',100)->nullable();
+            $table->string('status_tes',20);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('absen_sortirs');
+    }
+}
