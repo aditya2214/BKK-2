8792abbaa5559d6f29370213374b4d5667207796
aditya2214 <aditokta7333@gmail.com>
2021-10-14 11:32:21 +0700

    dfdfd

--- app/Attendance.php
+++ app/Attendance.php
@@ -26,6 +26,6 @@ class Attendance extends Model
     ];
 
     public function Seleksi_r(){
-        return $this->belongsTo(absenSortir::class. 'id');
+        return $this->belongsTo(\App\absenSortir::class. 'id');
     }
 }
