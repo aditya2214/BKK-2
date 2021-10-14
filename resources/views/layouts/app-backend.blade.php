1f8d2bd4a4b477b5803204ed3f11eb81cf3d44bf
aditya2214 <aditokta7333@gmail.com>
2021-10-14 08:37:37 +0700

    livewire add

--- resources/views/layouts/app-backend.blade.php
+++ resources/views/layouts/app-backend.blade.php
@@ -115,8 +115,8 @@
     <!-- select 2 -->
     <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
 
-    @yield('scripts')
     @livewireScripts
+    @yield('scripts')
 </body>
 
 </html>
