a7ecb63b632ee897229092945f4ceae9f9b36d6f
aditya2214 <aditokta7333@gmail.com>
2021-10-14 11:42:12 +0700

    cscdfd

--- app/absenSortir.php
+++ app/absenSortir.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class absenSortir extends Model
 {
-    //
+    protected $fillable = [
+        'id_peserta',
+        'kategori_tes',
+        'status_tes',
+    ];
+
+    public function Seleksi_r(){
+        return $this->belongsTo(\App\absenSortir::class,'id_peserta','id');
+    }
 }
