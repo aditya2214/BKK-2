9c14871636976e6514e12bfb9d9f1af4f7a657fb
aditya2214 <aditokta7333@gmail.com>
2021-10-14 09:34:24 +0700

    5ytythg

--- resources/views/layouts/app-frontend.blade.php
+++ resources/views/layouts/app-frontend.blade.php
@@ -30,7 +30,6 @@
     <!-- Custom styles for this template -->
     <link href="/frontend/css/style.css" rel="stylesheet" id="style">
     <script src="/ckeditor/ckeditor.js"></script>
-    @livewireStyles
 </head>
 
 <body class="body-scroll d-flex flex-column h-100 menu-overlay" data-page="homepage">
@@ -128,7 +127,6 @@
 
     <!-- page level custom script -->
     <script src="/frontend/js/app.js"></script>
-     @livewireScripts
 </body>
 
 </html>
