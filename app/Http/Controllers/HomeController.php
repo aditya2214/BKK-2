4a2577d8987e288b369bafcc0cee52c15b863b07
aditya2214 <aditokta7333@gmail.com>
2021-10-14 13:39:58 +0700

    livewire add

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -65,7 +65,7 @@ class HomeController extends Controller
 
         $vacancy = \App\Vacancy::all();
 
-        return view('back.seleksi_peserta',compact('vacancy'));
+        return view('back.seleksi_peserta',compact('vacancy','get_attendances','pilih_loker'));
     }
 
     public function select_loker(Request $request){
@@ -74,14 +74,12 @@ class HomeController extends Controller
 
         $pilih_loker = $this->id_loker;
         $vacancy = \App\Vacancy::all();
-        $get_attendances = \App\Attendance::where('id_vacancy',$this->id_loker)->get();
-        return view('back.seleksi_peserta',compact('vacancy','get_attendances','pilih_loker'));
+        return view('back.seleksi_peserta',compact('vacancy','pilih_loker'));
     }
 
     public function seleksi(Request $request){
     
         return $request->all();
-        
         $id_peserta = $request->checked;
 
         foreach ($id_peserta as $key => $value) {
