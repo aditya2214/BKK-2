ae03a8aa90ddae019555415196cfb77c7bb2a489
aditya2214 <aditokta7333@gmail.com>
2021-10-14 10:13:23 +0700

    yntuiy

--- resources/views/back/seleksi_peserta.blade.php
+++ resources/views/back/seleksi_peserta.blade.php
@@ -39,54 +39,66 @@
                 <p class="text-danger">Pilih Loker Terlebih Dulu</p>
                 @else
                 <p><b>Daftar Peserta</b></p>
-                <table class="table" id="dataTable">
-                    <thead>
-                        <tr>
-                            <th>
-                                <input type="checkbox" name="" id="">
-                            </th>
-                            <th>No</th>
-                            <th>Nama_Lengkap</th>
-                            <th>Jenis_Kelamin</th>
-                            <th>Email</th>
-                            <th>NIK</th>
-                            <th>Kabupaten</th>
-                            <th>Alamat</th>
-                            <th>TTL</th>
-                            <th>Umur</th>
-                            <th>Tinggi_Badan</th>
-                            <th>Berat_Badan</th>
-                            <th>Asal_Sekolah</th>
-                            <th>Tahun_lulus</th>
-                            <th>Pengalaman</th>
-                            <th>No_Handphone</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        @foreach($get_attendances as $key=>$get_attendance)
-                        <tr>
-                            <td>
-                                <input type="checkbox" name="checked" value="{{$get_attendance->id}}" id="checked">
-                            </td>
-                            <td>{{$key+1}}</td>
-                            <td>{{$get_attendance->full_name}}</td>
-                            <td>{{$get_attendance->gender}}</td>
-                            <td>{{$get_attendance->active_email}}</td>
-                            <td>{{$get_attendance->nik}}</td>
-                            <td>{{$get_attendance->kabupaten}}</td>
-                            <td>{!!$get_attendance->address!!}</td>
-                            <td>{{$get_attendance->place}},{{$get_attendance->date_and_place}}</td>
-                            <td>{{$get_attendance->age}}</td>
-                            <td>{{$get_attendance->height}}</td>
-                            <td>{{$get_attendance->weight}}</td>
-                            <td>{{$get_attendance->school}}</td>
-                            <td>{{$get_attendance->graduation_year}}</td>
-                            <td>{!!$get_attendance->experience!!}</td>
-                            <td>{{$get_attendance->no_handphone}}</td>
-                        </tr>
-                        @endforeach
-                    </tbody>
-                </table>
+                <form action="">
+                    
+                    <table class="table" id="dataTable">
+                        <thead>
+                            <tr>
+                                <th>
+                                    <input type="checkbox" name="" id="">
+                                </th>
+                                <th>No</th>
+                                <th>Nama_Lengkap</th>
+                                <th>Jenis_Kelamin</th>
+                                <th>Email</th>
+                                <th>NIK</th>
+                                <th>Kabupaten</th>
+                                <th>Alamat</th>
+                                <th>TTL</th>
+                                <th>Umur</th>
+                                <th>Tinggi_Badan</th>
+                                <th>Berat_Badan</th>
+                                <th>Asal_Sekolah</th>
+                                <th>Tahun_lulus</th>
+                                <th>Pengalaman</th>
+                                <th>No_Handphone</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach($get_attendances as $key=>$get_attendance)
+                            <tr>
+                                <td>
+                                    <input type="checkbox" name="checked" value="{{$get_attendance->id}}" id="checked">
+                                </td>
+                                <td>{{$key+1}}</td>
+                                <td>{{$get_attendance->full_name}}</td>
+                                <td>{{$get_attendance->gender}}</td>
+                                <td>{{$get_attendance->active_email}}</td>
+                                <td>{{$get_attendance->nik}}</td>
+                                <td>{{$get_attendance->kabupaten}}</td>
+                                <td>{!!$get_attendance->address!!}</td>
+                                <td>{{$get_attendance->place}},{{$get_attendance->date_and_place}}</td>
+                                <td>{{$get_attendance->age}}</td>
+                                <td>{{$get_attendance->height}}</td>
+                                <td>{{$get_attendance->weight}}</td>
+                                <td>{{$get_attendance->school}}</td>
+                                <td>{{$get_attendance->graduation_year}}</td>
+                                <td>{!!$get_attendance->experience!!}</td>
+                                <td>{{$get_attendance->no_handphone}}</td>
+                            </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                <hr>
+                <div class="form-group">
+                    <label for="">Tes Apa?</label>
+                    <input type="text" name="tes_apa" id="tes_apa" class="form-control">
+                </div>
+                <div class="form-group">
+                    <button class="btn btn-success">Lolos</button>
+                    <button class="btn btn-Danger">Gagal</button>
+                </div>
+                </form>
                 @endif
             </div>
         </div>
