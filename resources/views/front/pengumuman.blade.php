517f03ca50c9a43fd0e1dc7ac9324bacdb1c5d23
aditya2214 <aditokta7333@gmail.com>
2021-10-15 10:56:47 +0700

    dfdfsg

--- resources/views/front/pengumuman.blade.php
+++ resources/views/front/pengumuman.blade.php
@@ -90,10 +90,7 @@ li {
                         </ul>
                     </div>
                     <div class="card-body ">
-                        <p class="mb-0 text-mute f-sm"><b>{{$get_attendance->vacancy_r->title_vacancy}}</b></p>
-                    </div>
-                    <div class="card-footer">
-                        <button class="btn btn-primary"> Footer Button</button>
+                        <p class="mb-0 text-mute f-sm"><b>{{$get_attendance->vacancy_r->title_vacancy}}&nbsp;{{$get_attendance->vacancy_r->kategori_tes}}</b></p>
                     </div>
                     @endforeach
                     @endif
