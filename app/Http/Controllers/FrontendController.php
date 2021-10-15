e4af2b40b5c4142d0e852fd126d3d002af43d0fa
aditya2214 <aditokta7333@gmail.com>
2021-10-15 11:30:03 +0700

    dfdfsg

--- app/Http/Controllers/FrontendController.php
+++ app/Http/Controllers/FrontendController.php
@@ -81,10 +81,10 @@ class FrontendController extends Controller
         $this->searching = $request->search;
         $search_cek = $this->searching;
         // return $search_cek;
-        $get_attendances = \App\Attendance::where('nik','like','%'.$request->searching.'%')->orWhere('active_email','like','%'.$request->searching.'%')->orWhere('no_handphone','like','%'.$request->searching.'%')->get();
+        $get_attendances = \App\Attendance::where('nik','like','%'.$request->search.'%')->orWhere('active_email','like','%'.$request->search.'%')->orWhere('no_handphone','like','%'.$request->search.'%')->get();
 
         return $get_attendances;
-        
+
         return view('front.pengumuman',compact('get_attendances','search_cek'));
 
     }
