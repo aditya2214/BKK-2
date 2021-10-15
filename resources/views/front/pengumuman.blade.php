5b162b95e01f06ce7cbb59fe486a8d809e3ba55d
aditya2214 <aditokta7333@gmail.com>
2021-10-15 11:37:56 +0700

    dfdfsg

--- resources/views/front/pengumuman.blade.php
+++ resources/views/front/pengumuman.blade.php
@@ -85,7 +85,7 @@ li {
                     <div class="container">
                         <ul class="progressbar">
                             @foreach($get_attendance->Seleksi_r as $kal)
-                            @if($kal->status_test == "gagal")
+                            @if($kal->status_tes == "gagal")
                             <li><a href="#" class="text-danger" data-toggle="popover" data-placement="bottom" title="Popover Header" data-content="Some content inside the popover">{{$kal->status_tes}}: {{$kal->kategori_tes}}</a></li>
                             @else
                             <li><a href="#" class="active" data-toggle="popover" data-placement="bottom" title="Popover Header" data-content="Some content inside the popover">{{$kal->status_tes}}: {{$kal->kategori_tes}}</a></li>
