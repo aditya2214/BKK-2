3ab2b4c708be447b3ff46ee9c149ebe3fa9d7372
aditya2214 <aditokta7333@gmail.com>
2021-10-15 20:34:56 +0700

    ffdhdhg

--- app/Vacancy.php
+++ app/Vacancy.php
@@ -7,4 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 class Vacancy extends Model
 {
     protected $fillable = ['kode_vacancy','title_vacancy','place_and_date','recruitment','notes'];
+
+    public function attendance_r(){
+        return $this->hasMany(\App\Attendance::class,'id_vacancy','id')->count();
+    }
 }
