c813925edcebaf68791498e7c35ecf6b1e3a0422
aditya2214 <aditokta7333@gmail.com>
2021-10-16 22:33:37 +0700

    vgfdfsgf

--- database/factories/BookFactory.php
+++ database/factories/BookFactory.php
@@ -7,20 +7,20 @@ use Faker\Generator as Faker;
 
 $factory->define(\App\Attendance::class, function (Faker $faker) {
     return [
-        'id_vacancy' => $i,
+        'id_vacancy' => $faker->numberBetween(1,2),
         'full_name'  => $faker->name,
-        'gender' => $faker->name($gender),
+        'gender' =>  $faker->randomElement(['Pria','Wanita']),
         'active_email' => $faker->safeEmail,
         'nik' =>  $faker->randomDigit,
         'kabupaten' => $faker->word,
         'address' => $faker->address,
-        'place' => $faker->address,
+        'place' => $faker->state,
         'date_and_place' => $faker->date($format = 'Y-m-d', $max = 'now'),
         'age' => $faker->numberBetween(19,23),
         'height' => $faker->numberBetween(165,170),
         'weight' => $faker->numberBetween(65,70),
         'school' => $faker->randomElement(['SMK PGRI 2 KARAWANG','SMK 1 KARAWANG']),
-        'graduation_year' => $faker->date($format = 'Y', $max = 'now'),
+        'graduation_year' => $faker->date($format = 'Y-m-d', $max = 'now'),
         'experience' => $faker->word,
         'no_handphone' => $faker->phoneNumber
     ];
