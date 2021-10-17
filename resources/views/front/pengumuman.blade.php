82739e23b59355f2de80d4d34a5b7a771cd55566
aditya2214 <aditokta7333@gmail.com>
2021-10-17 13:53:21 +0700

    vgfdfsgf

--- resources/views/front/pengumuman.blade.php
+++ resources/views/front/pengumuman.blade.php
@@ -15,81 +15,7 @@
                             <button class="btn btn-primary form-control btn-sm">Searching</button>
                         </form>
                     </div>
-                    <style>
-         .events li { 
-  display: flex; 
-  color: #999;
-}
-
-.events time { 
-  position: relative;
-  padding: 0 1.5em;  }
-
-.events time::after { 
-   content: "";
-   position: absolute;
-   z-index: 2;
-   right: 0;
-   top: 0;
-   transform: translateX(50%);
-   border-radius: 50%;
-   background: #fff;
-   border: 1px #ccc solid;
-   width: .8em;
-   height: .8em;
-}
-
-
-.events span {
-  padding: 0 1.5em 1.5em 1.5em;
-  position: relative;
-}
-
-.events span::before {
-   content: "";
-   position: absolute;
-   z-index: 1;
-   left: 0;
-   height: 100%;
-   border-left: 1px #ccc solid;
-}
-
-.events strong {
-   display: block;
-   font-weight: bolder;
-}
-
-.events { margin: 1em; width: 50%; }
-.events, 
-.events *::before, 
-.events *::after { box-sizing: border-box; font-family: arial; }
-
-                    </style>
-                    <ul class="events">
-  <li>
-    <time datetime="10:03">10:03</time> 
-    <span><strong>Bat &amp; Ball</strong> On time</span></li>
-    
-  <li>
-    <time datetime="10:03">10:03</time> 
-    <span><strong>Bat &amp; Ball</strong> On time</span></li>
-  
-  <li>
-    <time datetime="10:03">10:03</time> 
-    <span><strong>Bat &amp; Ball</strong> On time and other text that may span over 2 lines</span></li>
-  
-  <li>
-    <time datetime="10:03">10:03</time> 
-    <span><strong>Bat &amp; Ball</strong> On time</span></li>
-  
-  <li>
-    <time datetime="10:03">10:03</time> 
-    <span><strong>Bat &amp; Ball</strong> On time</span></li>
-  
-  <li>
-    <time datetime="10:03">10:03</time> 
-    <span><strong>Bat &amp; Ball</strong> On time</span></li>
-</ul>
+                    
                     @if($search_cek == null)
 
                     @else
@@ -102,103 +28,68 @@
                     </ul>
                     <br><br>
                     <style>
-                      .stepper-wrapper {
-                        margin-top: auto;
-                        display: flex;
-                        justify-content: space-between;
-                        margin-bottom: 20px;
+                      .events li { 
+                        display: flex; 
+                        color: #999;
                       }
-                      .stepper-item {
+
+                      .events time { 
                         position: relative;
-                        display: flex;
-                        flex-direction: column;
-                        align-items: center;
-                        flex: 1;
+                        padding: 0 1.5em;  }
 
-                        @media (max-width: 768px) {
-                          font-size: 12px;
-                        }
-                      }
-
-                      .stepper-item::before {
-                        position: absolute;
+                      .events time::after { 
                         content: "";
-                        border-bottom: 2px solid #ccc;
-                        width: 100%;
-                        top: 20px;
-                        left: -50%;
-                        z-index: 2;
-                      }
-
-                      .stepper-item::after {
                         position: absolute;
-                        content: "";
-                        border-bottom: 2px solid #ccc;
-                        width: 100%;
-                        top: 20px;
-                        left: 50%;
                         z-index: 2;
-                      }
-
-                      .stepper-item .step-counter {
-                        position: relative;
-                        z-index: 5;
-                        display: flex;
-                        justify-content: center;
-                        align-items: center;
-                        width: 40px;
-                        height: 40px;
+                        right: 0;
+                        top: 0;
+                        transform: translateX(50%);
                         border-radius: 50%;
-                        background: #ccc;
-                        margin-bottom: 6px;
+                        background: #fff;
+                        border: 1px #ccc solid;
+                        width: .8em;
+                        height: .8em;
                       }
 
-                      .stepper-item.active {
-                        font-weight: bold;
-                        color:green;
+
+                      .events span {
+                        padding: 0 1.5em 1.5em 1.5em;
+                        position: relative;
                       }
 
-                      .stepper-item.unactive {
-                        font-weight: bold;
-                        color:red;
-                      }
-
-                      .stepper-item.completed .step-counter {
-                        background-color: #4bb543;
-                      }
-
-                      .stepper-item.completed::after {
-                        position: absolute;
+                      .events span::before {
                         content: "";
-                        border-bottom: 2px solid #4bb543;
-                        width: 100%;
-                        top: 20px;
-                        left: 50%;
-                        z-index: 3;
+                        position: absolute;
+                        z-index: 1;
+                        left: 0;
+                        height: 100%;
+                        border-left: 1px #ccc solid;
                       }
 
-                      .stepper-item:first-child::before {
-                        content: none;
+                      .events strong {
+                        display: block;
+                        font-weight: bolder;
                       }
-                      .stepper-item:last-child::after {
-                        content: none;
-                      }
+
+                      .events { margin: 1em; width: 50%; }
+                      .events, 
+                      .events *::before, 
+                      .events *::after { box-sizing: border-box; font-family: arial; }
+
                     </style>
-                    <div class="stepper-wrapper">
+                    <ul class="events">
                       @foreach($get_attendance->Seleksi_r as $key=>$kal)
-                      @if($kal->status_tes == "gagal")
-                      <div class="stepper-item unactive">
-                        <div class="step-counter">{{$key+1}}</div>
-                        <div class="step-name">{{$kal->status_tes}}: {{$kal->kategori_tes}}</div>
-                      </div>
-                      @else
-                      <div class="stepper-item active">
-                        <div class="step-counter">{{$key+1}}</div>
-                        <div class="step-name">{{$kal->status_tes}}: {{$kal->kategori_tes}}</div>
-                      </div>
-                      @endif
+                        @if($kal->status_tes == "gagal")
+                        <li>
+                          <time datetime="10:03">{{$kal->createrd_at}} | {{$key+1}}</time> 
+                          <span><strong class="text-danger">{{$kal->status_tes}}: {{$kal->kategori_tes}}</strong> On time</span></li>
+                        @else
+                          <li>
+                          <time datetime="10:03">10:03</time> 
+                          <span><strong class="text-success">{{$kal->status_tes}}: {{$kal->kategori_tes}}</strong> On time</span></li>
+                        @endif
                       @endforeach
-                    </div>
+                    </ul>
                     <hr>
                     @endforeach
                     @endif
