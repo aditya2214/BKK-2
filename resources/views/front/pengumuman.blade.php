a2720f36494b133e89125b0e1b208d0403a1eb21
aditya2214 <aditokta7333@gmail.com>
2021-10-17 13:38:58 +0700

    vgfdfsgf

--- resources/views/front/pengumuman.blade.php
+++ resources/views/front/pengumuman.blade.php
@@ -1,68 +1,6 @@
 @extends('layouts.app-frontend')
 
 @section('content')
-<style>
-    .container {
-  width: 100%;
-}
-
-.progressbar {
-  counter-reset: step;
-}
-
-.progressbar li a{
-  list-style-type: none;
-  float: left;
-  width: 33.33%;
-  position: relative;
-  text-align: center;
-}
-
-.progressbar li a:before {
-  content: counter(step);
-  counter-increment: step;
-  width: 30px;
-  height: 30px;
-  line-height: 30px;
-  border: 2px solid #ddd;
-  display: block;
-  text-align: center;
-  margin: 0 auto 10px auto;
-  border-radius: 50%;
-  background-color: white;
-}
-
-.progressbar li a:after {
-  content: '';
-  position: absolute;
-  width: 100%;
-  height: 5px;
-  background-color: #ddd;
-  top: 15px;
-  left: -50%;
-  z-index: -1;
-}
-
-.progressbar li:first-child a:after {
-  content: none;
-}
-
-.progressbar li a.active {
-  color: green;
-}
-
-.progressbar li a.active:before {
-  border-color: green;
-}
-
-.progressbar li a.none-active:after {
-  background-color:green !important;
-}
-li {
-  list-style:none;
-    text-align: -webkit-match-parent;
-}
-</style>
 <div class="main-container">
     <div class="container">
         <div class="row">
@@ -76,6 +14,111 @@ li {
                             <br>
                             <button class="btn btn-primary form-control btn-sm">Searching</button>
                         </form>
+                    </div>
+                    <style>
+                      .wrapper {
+  width: 330px;
+  font-family: 'Helvetica';
+  font-size: 14px;
+  border: 1px solid #CCC;
+}
+
+.StepProgress {
+  position: relative;
+  padding-left: 45px;
+  list-style: none;
+  
+  &::before {
+    display: inline-block;
+    content: '';
+    position: absolute;
+    top: 0;
+    left: 15px;
+    width: 10px;
+    height: 100%;
+    border-left: 2px solid #CCC;
+  }
+  
+  &-item {
+    position: relative;
+    counter-increment: list;
+    
+    &:not(:last-child) {
+      padding-bottom: 20px;
+    }
+    
+    &::before {
+      display: inline-block;
+      content: '';
+      position: absolute;
+      left: -30px;
+      height: 100%;
+      width: 10px;
+    }
+    
+    &::after {
+      content: '';
+      display: inline-block;
+      position: absolute;
+      top: 0;
+      left: -37px;
+      width: 12px;
+      height: 12px;
+      border: 2px solid #CCC;
+      border-radius: 50%;
+      background-color: #FFF;
+    }
+    
+    &.is-done {
+      &::before {
+        border-left: 2px solid green;
+      }
+      &::after {
+        content: "✔";
+        font-size: 10px;
+        color: #FFF;
+        text-align: center;
+        border: 2px solid green;
+        background-color: green;
+      }
+    }
+    
+    &.current {
+      &::before {
+        border-left: 2px solid green;
+      }
+      
+      &::after {
+        content: counter(list);
+        padding-top: 1px;
+        width: 19px;
+        height: 18px;
+        top: -4px;
+        left: -40px;
+        font-size: 14px;
+        text-align: center;
+        color: green;
+        border: 2px solid green;
+        background-color: white;
+      }
+    }
+  }
+  
+  strong {
+    display: block;
+  }
+}
+                    </style>
+                    <div class="wrapper">
+                    <ul class="StepProgress">
+                      <li class="StepProgress-item is-done"><strong>Post a contest</strong></li>
+                      <li class="StepProgress-item is-done"><strong>Award an entry</strong>
+                        Got more entries that you love? Buy more entries anytime! Just hover on your favorite entry and click the Buy button
+                      </li>
+                      <li class="StepProgress-item current"><strong>Post a contest</strong></li>
+                      <li class="StepProgress-item"><strong>Handover</strong></li>
+                      <li class="StepProgress-item"><strong>Provide feedback</strong></li>
+                    </ul>
                     </div>
                     @if($search_cek == null)
 
@@ -145,6 +188,11 @@ li {
                         color:green;
                       }
 
+                      .stepper-item.unactive {
+                        font-weight: bold;
+                        color:red;
+                      }
+
                       .stepper-item.completed .step-counter {
                         background-color: #4bb543;
                       }
@@ -169,7 +217,7 @@ li {
                     <div class="stepper-wrapper">
                       @foreach($get_attendance->Seleksi_r as $key=>$kal)
                       @if($kal->status_tes == "gagal")
-                      <div class="stepper-item">
+                      <div class="stepper-item unactive">
                         <div class="step-counter">{{$key+1}}</div>
                         <div class="step-name">{{$kal->status_tes}}: {{$kal->kategori_tes}}</div>
                       </div>
