94213e93d78c3ab43241bbf1db0749dfdbb4cf89
aditya2214 <aditokta7333@gmail.com>
2021-10-18 11:29:18 +0700

    livewire add

--- resources/views/front/home.blade.php
+++ resources/views/front/home.blade.php
@@ -166,30 +166,30 @@
                     <div class="row justify-content-equal no-gutters">
                         <div class="col-4 col-md-2 mb-3">
                             <div class="icon icon-50 rounded-circle mb-1 bg-default-light text-default"><span class="material-icons">qr_code_2</span></div>
-                            <p class="text-secondary"><small>Pay</small></p>
+                            <p class="text-secondary"><small>KIIC</small></p>
                         </div>
                         <div class="col-4 col-md-2 mb-3">
                             <div class="icon icon-50 rounded-circle mb-1 bg-default-light text-default"><span class="material-icons">swap_horiz</span></div>
-                            <p class="text-secondary"><small>Transfer</small></p>
+                            <p class="text-secondary"><small>KIM</small></p>
                         </div>
                         <div class="col-4 col-md-2 mb-3">
                             <div class="icon icon-50 rounded-circle mb-1 bg-default-light text-default"><span class="material-icons">sim_card</span></div>
-                            <p class="text-secondary"><small>Reacharge</small></p>
+                            <p class="text-secondary"><small>SURYA CIPTA</small></p>
                         </div>
                         <div class="col-4 col-md-2 mb-3">
                             <div class="icon icon-50 rounded-circle mb-1 bg-default-light text-default"><span class="material-icons">account_circle</span></div>
-                            <p class="text-secondary"><small>Send</small></p>
+                            <p class="text-secondary"><small>INDOTAISEI</small></p>
                         </div>
                         <div class="col-4 col-md-2 mb-3">
                             <div class="icon icon-50 rounded-circle mb-1 bg-default-light text-default"><span class="material-icons">receipt</span></div>
-                            <p class="text-secondary"><small>Bill</small></p>
+                            <p class="text-secondary"><small>BIC</small></p>
                         </div>
                         <div class="col-4 col-md-2 mb-3">
                             <div class="icon icon-50 rounded-circle mb-1 bg-default-light text-default"><span class="material-icons">wb_incandescent</span></div>
-                            <p class="text-secondary"><small>Electricity</small></p>
+                            <p class="text-secondary"><small>MM2100</small></p>
                         </div>
                     </div>
-                    <button class="btn btn-sm btn-outline-secondary rounded" id="more-expand-btn">Show more <span class="ml-2 small material-icons">expand_more</span></button>
+                    <!-- <button class="btn btn-sm btn-outline-secondary rounded" id="more-expand-btn">Show more <span class="ml-2 small material-icons">expand_more</span></button>
                     <div class="row justify-content-equal no-gutters" id="more-expand">
                         <div class="col-4 col-md-2">
                             <div class="icon icon-50 rounded-circle mb-1 bg-default-light text-default"><span class="material-icons">beach_access</span></div>
@@ -203,7 +203,7 @@
                             <div class="icon icon-50 rounded-circle mb-1 bg-default-light text-default"><span class="material-icons">flight</span></div>
                             <p class="text-secondary"><small>Flight</small></p>
                         </div>
-                    </div>
+                    </div> -->
                 </div>
             </div>
         </div>
