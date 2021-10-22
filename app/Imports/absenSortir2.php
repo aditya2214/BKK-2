d16aec35fdaeaeb40f2b35289edf7b6e9dad2ea0
aditya2214 <aditokta7333@gmail.com>
2021-10-22 19:52:45 +0700

    nama

--- app/Imports/absenSortir2.php
+++ app/Imports/absenSortir2.php
@@ -15,15 +15,10 @@ class absenSortir2 implements ToModel,WithHeadingRow
     */
     public function model(array $row)
     {
-        $subjectVal  = $row['status'];
-        $searchVal = array("T"," ", "G"," ");
-        $replaceVal = array("t", "", "g", "");
-        $stat = str_replace($searchVal, $replaceVal, $subjectVal);
-
         return new absenSortir([
             'id' => $row['id'],
             'id_peserta' => $row['id'],
-            'status_tes' => $stat,
+            'status_tes' => $row['status'],
             'kategori_tes' => $row['kategori_tes'],
             'notes2' => $row['catatan']
         ]);
