49aeb92e6adf6f3ffc3a57bbdc03727a9f307290
aditya2214 <aditokta7333@gmail.com>
2021-10-22 18:59:27 +0700

    nama

--- resources/views/front/pengumuman.blade.php
+++ resources/views/front/pengumuman.blade.php
@@ -79,7 +79,7 @@
                     </style>
                     <ul class="events">
                       @foreach($get_attendance->Seleksi_r as $key=>$kal)
-                       if($kal->status_tes == "gagal")
+                       @if($kal->status_tes == "gagal")
                         <li>
                           <p><b class="text-danger"></b></p> 
                           <span>{{date('M d',strtotime($kal->created_at))}}<strong class="text-danger">{{$kal->status_tes}}: {{$kal->kategori_tes}}</strong><br><small>{!!$kal->notes2!!}</small></span></li>
