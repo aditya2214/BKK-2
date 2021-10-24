56f1fe41546228f894e58e92e721b6758c8841f0
aditya2214 <aditokta7333@gmail.com>
2021-10-24 22:23:28 +0700

    sdgsfgs

--- resources/views/front/pdf_pendaftaran.blade.php
+++ resources/views/front/pdf_pendaftaran.blade.php
@@ -9,10 +9,6 @@
 
     <link href="/backend/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
     <link href="/backend/vendor/datatables/dataTables.bootstrap4.min.css" rel="stylesheet">
-    <link
-        href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
-        rel="stylesheet">
-
         <script src="/ckeditor/ckeditor.js"></script>
     <!-- Custom styles for this template-->
     <link href="/backend/css/sb-admin-2.min.css" rel="stylesheet">
