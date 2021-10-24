f38a9ac3baaa0e512acf7e5c8c557f622860e08b
aditya2214 <aditokta7333@gmail.com>
2021-10-24 22:26:23 +0700

    sdgsfgs

--- resources/views/front/pdf_pendaftaran.blade.php
+++ resources/views/front/pdf_pendaftaran.blade.php
@@ -7,11 +7,7 @@
     <title>Invoice</title>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
 
-    <link href="/backend/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
-    <link href="/backend/vendor/datatables/dataTables.bootstrap4.min.css" rel="stylesheet">
-        <script src="/ckeditor/ckeditor.js"></script>
-    <!-- Custom styles for this template-->
-    <link href="/backend/css/sb-admin-2.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
    
 </head>
 <body>
@@ -49,9 +45,9 @@
 
 
     <!-- script -->
-    <script src="/backend/vendor/jquery/jquery.min.js"></script>
-    <script src="/backend/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-    <script src="/backend/js/sb-admin-2.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 </body>
 
 </html>
