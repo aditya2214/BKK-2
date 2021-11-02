c3fdf6fc879f734eb346623cea16695a48905adc
aditya2214 <aditokta7333@gmail.com>
2021-11-02 16:23:51 +0700

    nama

--- routes/web.php
+++ routes/web.php
@@ -50,7 +50,7 @@ Route::get('/mitra_bkk', 'HomeController@mitra_bkk')->name('mitra_bkk');
 Route::post('/store_mitra', 'HomeController@store_mitra')->name('store_mitra');
 Route::get('/whatsapp/{id}', 'HomeController@whatsapp')->name('whatsapp');
 Route::post('/siswa/import_excel', 'HomeController@import_excel');
-Route::post('/test_online', 'HomeController@test_online');
+Route::get('/test_online', 'HomeController@test_online');
 
 Route::get('/switch_01/{id}', 'HomeController@switch_01');
 Route::get('/switch_02/{id}', 'HomeController@switch_02');
