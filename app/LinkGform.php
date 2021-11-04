24b105c5af59d978361598a01c7cf3a14107556e
aditya2214 <aditokta7333@gmail.com>
2021-11-04 11:13:12 +0700

    nama

--- app/LinkGform.php
+++ app/LinkGform.php
@@ -7,4 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 class LinkGform extends Model
 {
     protected $fillable = ['id_loker','link_gfom'];
+
+    public function vacancy_rr(){
+        return $this->belongsTo(\App\Vacancy::class,'id','id_loker');
+    }
 }
