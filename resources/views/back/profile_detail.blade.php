80873aef4dee9785908608fe48ad0fa8b1844392
aditya2214 <aditokta7333@gmail.com>
2021-11-06 22:44:37 +0700

    gdfhdgfhdf

--- resources/views/back/profile_detail.blade.php
+++ resources/views/back/profile_detail.blade.php
@@ -90,10 +90,7 @@
             <div class="card p-3 py-4">
                 <div class="text-center"> <img src="https://i.imgur.com/bDLhJiP.jpg" width="100" class="rounded-circle"> </div>
                 <div class="text-center mt-3"> <span class="bg-secondary p-1 px-4 rounded text-white">{{Auth::user()->email}}</span>
-                    <h5 class="mt-2 mb-0">{{Auth::user()->name}}</h5> <span>UI/UX Designer</span>
-                    <div class="px-4 mt-1">
-                        <p class="fonts">Consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
-                    </div>
+                    <h5 class="mt-2 mb-0">{{Auth::user()->name}}</h5>
                     <ul class="social-list">
                         <li><i class="fa fa-facebook"></i></li>
                         <li><i class="fa fa-dribbble"></i></li>
@@ -101,7 +98,12 @@
                         <li><i class="fa fa-linkedin"></i></li>
                         <li><i class="fa fa-google"></i></li>
                     </ul>
-                    <div class="buttons"> <a class="btn btn-outline-primary px-4">Whatsapp</a> <button class="btn btn-primary px-4 ms-3">Contact</button> </div>
+                    <div class="buttons">
+                        <a class="btn btn-outline-primary px-4"><i class="fab fa-whatsapp fa-3x"></i></a> 
+                        <a class="btn btn-primary px-4 ms-3"><i class="fab fa-facebook-f fa-3x"></i></a>
+                        <a class="btn btn-primary px-4 ms-3"><i class="fab fa-instagram fa-3x"></i></a>
+                     
+                    </div>
                 </div>
             </div>
         </div>
