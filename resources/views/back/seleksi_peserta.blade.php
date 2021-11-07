66cc965c419cfbd5e969cd74e9ec32abc153e280
aditya2214 <aditokta7333@gmail.com>
2021-11-07 22:13:26 +0700

    fdsgfdsgdfg

--- resources/views/back/seleksi_peserta.blade.php
+++ resources/views/back/seleksi_peserta.blade.php
@@ -177,12 +177,7 @@
 <script>
    $(document).ready(function() {
     $('#dataTable').DataTable( {
-        dom: 'Bfrtip',
-        buttons: [
-            'copyHtml5',
-            'excelHtml5',
-            'csvHtml5',
-        ]
+        dom: 'Bfrtip'
     } );
 } );
 
