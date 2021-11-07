7d0e94c9f042db0330f2444964111fe33be30409
aditya2214 <aditokta7333@gmail.com>
2021-11-07 18:08:32 +0700

    fdsgfdsgdfg

--- app/Http/Controllers/FrontendController.php
+++ app/Http/Controllers/FrontendController.php
@@ -152,7 +152,7 @@ class FrontendController extends Controller
 
         if ($cek_request == $cek_kode_vacancy->kode_vacancy) {
             Alert::success('Berhasil', 'Silahkan Isi Biodata Anda!!!');
-            return redirect('https://'.$link->link_gfom);
+            return "<a target='_blank' href='https://".$link->link_gfom."></a>";
         }else{
             Alert::error('Gagal', 'Kode Tes Salah!!!');
             return redirect()->back();
