7697c2d88990adc5755312b629b3ccc688ca7510
aditya2214 <aditokta7333@gmail.com>
2021-11-07 21:46:00 +0700

    fdsgfdsgdfg

--- app/Imports/PesertaImport.php
+++ app/Imports/PesertaImport.php
@@ -36,6 +36,48 @@ class PesertaImport implements ToModel,WithHeadingRow
 
     }
 
+    public static function ExcelToPHP($dateValue = 0)
+    {
+        $myExcelBaseDate = 25569;
+        //    Adjust for the spurious 29-Feb-1900 (Day 60)
+        if ($dateValue < 60) {
+            --$myExcelBaseDate;
+        }
+
+        // Perform conversion
+        if ($dateValue >= 1) {
+            $utcDays = $dateValue - $myExcelBaseDate;
+            $returnValue = round($utcDays * 86400);
+            if (($returnValue <= PHP_INT_MAX) && ($returnValue >= -PHP_INT_MAX)) {
+                $returnValue = (int) $returnValue;
+            }
+        } else {
+            $hours = round($dateValue * 24);
+            $mins = round($dateValue * 1440) - round($hours * 60);
+            $secs = round($dateValue * 86400) - round($hours * 3600) - round($mins * 60);
+            $returnValue = (int) gmmktime($hours, $mins, $secs);
+        }
+
+        // Return
+        return $returnValue;
+    }    //    function ExcelToPHP()
+
+    public static function ExcelToPHPObject($dateValue = 0)
+    {
+        $dateTime = self::ExcelToPHP($dateValue);
+        $days = floor($dateTime / 86400);
+        $time = round((($dateTime / 86400) - $days) * 86400);
+        $hours = round($time / 3600);
+        $minutes = round($time / 60) - ($hours * 60);
+        $seconds = round($time) - ($hours * 3600) - ($minutes * 60);
+
+        $dateObj = date_create('1-Jan-1970+' . $days . ' days');
+        $dateObj->setTime($hours, $minutes, $seconds);
+
+        return $dateObj;
+    }
+
+
     public function headingRow(): int
     {
         return 1;
