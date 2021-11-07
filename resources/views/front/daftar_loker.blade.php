f095339cd2991514026b4d0e7b0e0312228588ad
aditya2214 <aditokta7333@gmail.com>
2021-11-07 09:50:32 +0700

    fdsgfdsgdfg

--- resources/views/front/daftar_loker.blade.php
+++ resources/views/front/daftar_loker.blade.php
@@ -8,25 +8,28 @@
             <div class="card-body px-0">
                 <div class="list-group list-group-flush">
                  @foreach($vacancies as $vacancy)
-                    <a class="list-group-item" href="#">
-                        <div class="row">
-                            <div class="col-auto align-self-center">
-                            <i class="fas fa-thumbtack"></i>
-                            </div>
-                            <div class="col pl-0">
-                                <div class="row mb-1">
-                                    <div class="col">
-                                        <p class="mb-0">{{$vacancy->title_vacancy}}</p>
-                                    </div>
-                                    <div class="col-auto pl-0">
-                                        <p class="small text-secondary">Di Posting TGL :</p>
-                                        <p class="small text-secondary">{{$vacancy->created_at}}</p>
-                                    </div>
+                    <form action="{{ url ('masukan_kode/'.$vacancy->id) }} " method="get">
+                        <input type="hidden" name="tes_ol" value="{{$tes_online}}">
+                        <a class="list-group-item" href="">
+                            <div class="row">
+                                <div class="col-auto align-self-center">
+                                <i class="fas fa-thumbtack"></i>
                                 </div>
-                                <p class="small text-secondary"></p>
+                                <div class="col pl-0">
+                                    <div class="row mb-1">
+                                        <div class="col">
+                                            <p class="mb-0">{{$vacancy->title_vacancy}}</p>
+                                        </div>
+                                        <div class="col-auto pl-0">
+                                            <p class="small text-secondary">Di Posting TGL :</p>
+                                            <p class="small text-secondary">{{$vacancy->created_at}}</p>
+                                        </div>
+                                    </div>
+                                    <button class="small">Ikuti Tes</button>
+                                </div>
                             </div>
-                        </div>
-                    </a>
+                        </a>
+                    </form>
                 @endforeach
                 </div>
             </div>
