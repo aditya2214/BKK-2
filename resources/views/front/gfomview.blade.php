decd09df453aec91d31f9ab7f11633fb6c44795d
aditya2214 <aditokta7333@gmail.com>
2021-11-07 19:07:11 +0700

    fdsgfdsgdfg

--- resources/views/front/gfomview.blade.php
+++ resources/views/front/gfomview.blade.php
@@ -4,7 +4,7 @@
 
     <div class="main-container">
         <div class="row">
-            <div class="col-md-12">
+            <div class="col-md-6 offset-md-3">
                 <div class="card">
                     <div class="card-body">
                         {!!$link->link_gfom!!}
