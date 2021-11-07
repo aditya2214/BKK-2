3e3f6239561c484baad4cec3c98374aedbe7866d
aditya2214 <aditokta7333@gmail.com>
2021-11-07 10:39:16 +0700

    fdsgfdsgdfg

--- resources/views/front/daftar_loker.blade.php
+++ resources/views/front/daftar_loker.blade.php
@@ -8,7 +8,7 @@
             <div class="card-body px-0">
                 <div class="list-group list-group-flush">
                  @foreach($vacancies as $vacancy)
-                        <a class="list-group-item" href="">
+                        <div class="list-group-item" href="">
                             <div class="row">
                                 <div class="col-auto align-self-center">
                                 <i class="fas fa-thumbtack"></i>
@@ -46,7 +46,7 @@
                                     </div>
                                 </div>
                             </div>
-                        </a>
+                        </div>
                 @endforeach
                 </div>
             </div>
